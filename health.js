25a1c662d816b5d3f0b2e5296b29040f5ccd7c72
catmaikl <kotmajkl88@gmail.com>
2025-06-13 21:15:39 +0500

    Add files via upload

---
+++ health.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+  if (req.url === '/health') {
+    res.writeHead(200);
+    res.end('OK');
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+}).listen(8081);
