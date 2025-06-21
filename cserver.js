c73691555f09419b744a82ba167a7ce0f0422bec
catmaikl <kotmajkl88@gmail.com>
2025-06-21 15:38:32 +0500

    Update cserver.js

--- cserver.js
+++ cserver.js
@@ -3,6 +3,28 @@ const WebSocket = require('ws');
 const http = require('http');
 
 const app = express();
+
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    'https://cosatka-clickgame-277.netlify.app',
+    'http://localhost:3000'
+  ];
+  const origin = req.headers.origin;
+  
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  
+  next();
+});
+
 const server = http.createServer(app);
 
 // WebSocket-сервер
