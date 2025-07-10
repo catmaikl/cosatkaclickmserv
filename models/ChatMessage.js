d8a0e814acc603a27e86b91ab255014e5ae02053
catmaikl <kotmajkl88@gmail.com>
2025-07-10 10:53:54 +0500

    Update ChatMessage.js

--- models/ChatMessage.js
+++ models/ChatMessage.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const chatMessageSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Player', required: true },
+  userId: { type: String, ref: 'Player', required: true },
   username: { type: String, required: true },
   text: { type: String, required: true, maxlength: 200 },
   channel: { type: String, enum: ['global', 'clan', 'private'], default: 'global' },
